de577026b34d17d8c8b5ce30177bcf3a6f945578
Gabriel Pelletier <36201885+GabrielPelletier@users.noreply.github.com>
2023-02-01 14:23:06 -0500

    added full path to integrations.json

--- assets/js/filter_items.js
+++ assets/js/filter_items.js
@@ -1,5 +1,5 @@
 // File path where integrations/extensions/use cases are listed
-const file_path = 'integrations.json';
+const file_path = '/affiliates-jekyll-test-site/assets/js/integrations.json';
 
 // Create VueJS app to load `integrations.json` and allow filtering by name/tag
 var app = new Vue({
